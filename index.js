2bdf54dc08c0c6f941aa2a2347995ecb98362f9b
chisombiri <chisombiri@gmail.com>
2022-06-06 23:43:50 +0100

    implemented navbar toggle

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const menu = document.querySelector('.menu');
+const targets = document.querySelectorAll('.target');
 const icons = document.querySelectorAll('.section-1-icons i');
 let i = 1; //counter variable
+
+menu.addEventListener('click', () => {
+    targets.forEach((target) => {
+        target.classList.toggle('change');
+    });
+});
 
 setInterval(() => {
     i++
